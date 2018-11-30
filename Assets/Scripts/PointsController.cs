9e945d4d24c59c84c0bec15c6cb99a0b46f16036
Rebecca <rebecca.poulson@gmail.com>
2018-11-30 16:10:43 -0600

    log points from slider

---
+++ Assets/Scripts/PointsController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointsController : MonoBehaviour {
+
+    private float resourcePoints;
+
+    private float ghgPoints;
+
+    public void changePoints(Slider slider)
+    {
+        resourcePoints = slider.value;
+        ghgPoints = 30 - slider.value;
+        Debug.Log(resourcePoints);
+        Debug.Log(ghgPoints);
+    }
+
+
+
+    //set resource points when slider moves
+
+    //send resource points to the next scene
+
+
+}
