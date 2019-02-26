9f489bd794c2a34d655428432bee516f3fa93434
dylanjkennedy <44954471+dylanjkennedy@users.noreply.github.com>
2019-02-26 01:09:23 -0600

    Fish Scale based on GHG
    Lotta GHG means small fish cause they are dying

---
+++ Assets/Scripts/Fishies.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fishies : MonoBehaviour
+{
+    public GameObject FishContainer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < FishContainer.transform.childCount; i++)
+        {
+            FishContainer.transform.GetChild(i).gameObject.transform.localScale = Vector3.one * (1.0f / 2.0f)* Mathf.Sqrt(Mathf.Cos(PointsController.totalGhgPoints * Mathf.PI / 2 / 1000));
+            Debug.Log((PointsController.totalGhgPoints / 2000));
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
